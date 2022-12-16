88867817a8a64a60a63943096dae77d057322643
Lee Maguire <lee.maguire@mongodb.com>
2022-12-16 16:41:12 +0000

    Don't make SPM use local core package by default.

--- examples/SPM/Sources/SPMRealmCxx/main.cpp
+++ examples/SPM/Sources/SPMRealmCxx/main.cpp
@@ -1,8 +1,7 @@
 #include <chrono>
-#include <semaphore>
 #include <stdio.h>
+#include <future>
 #include <thread>
-#include "event_loop.hpp"
 #include <cpprealm/sdk.hpp>
 #include <realm/object-store/sync/sync_session.hpp>
 
@@ -15,55 +14,51 @@ struct FooObject: realm::object {
     realm::persisted<realm::uuid> uuid_col;
     realm::persisted<std::vector<std::uint8_t>> binary_col;
 
-    using schema = realm::schema<
-    "AllTypesObject",
-    realm::property<"_id", &FooObject::_id, true>,
-    realm::property<"bool_col", &FooObject::bool_col>,
-    realm::property<"str_col", &FooObject::str_col>,
-    realm::property<"date_col", &FooObject::date_col>,
-    realm::property<"uuid_col", &FooObject::uuid_col>,
-    realm::property<"binary_col", &FooObject::binary_col>>;
+    static constexpr auto schema = realm::schema(
+        "AllTypesObject",
+        realm::property<&FooObject::_id, true>("_id"),
+        realm::property<&FooObject::bool_col>("bool_col"),
+        realm::property<&FooObject::str_col>("str_col"),
+        realm::property<&FooObject::date_col>("date_col"),
+        realm::property<&FooObject::uuid_col>("uuid_col"),
+        realm::property<&FooObject::binary_col>("binary_col"));
 };
 
-realm::task<void> wait_for_sync_uploads(const realm::User& user) {
+std::promise<void> wait_for_sync_uploads(const realm::User& user) {
     auto sync_sessions = user.m_user->sync_manager()->get_all_sessions();
     auto session = sync_sessions[0];
-    co_await realm::make_awaitable<std::error_code>([&] (auto cb) {
-        session->wait_for_upload_completion(cb);
-    });
-    co_return;
+    std::promise<void> p;
+    session->wait_for_upload_completion([&p] (auto) { p.set_value(); });
+    return p;
 }
 
-realm::task<void> wait_for_sync_downloads(const realm::User& user) {
+std::promise<void> wait_for_sync_downloads(const realm::User& user) {
     auto sync_sessions = user.m_user->sync_manager()->get_all_sessions();
     auto session = sync_sessions[0];
-    co_await realm::make_awaitable<std::error_code>([&] (auto cb) {
-        session->wait_for_download_completion(cb);
-    });
-    co_return;
+    std::promise<void> p;
+    session->wait_for_download_completion([&p](auto) { p.set_value(); });
+    return p;
 }
 
-realm::task<realm::db_config> task;
-
-realm::task<realm::db_config> run_realm() {
+void run_realm() {
     auto app = realm::App("cpp-sdk-2-pgjiz");
-    auto user = co_await app.login(realm::App::Credentials::anonymous());
+    auto user = app.login(realm::App::Credentials::anonymous()).get_future().get();
 
     auto flx_sync_config = user.flexible_sync_configuration();
     std::cout << flx_sync_config.path << std::endl;
 
-    auto tsr = co_await realm::async_open<FooObject>(flx_sync_config);
+    auto tsr = realm::async_open<FooObject>(flx_sync_config).get_future().get();
     auto synced_realm = tsr.resolve();
 
-    auto update_success = co_await synced_realm.subscriptions().update([](realm::MutableSyncSubscriptionSet& subs) {
+    auto update_success = synced_realm.subscriptions().update([](realm::MutableSyncSubscriptionSet& subs) {
         subs.clear();
-    });
+    }).get_future().get();
 
-    update_success = co_await synced_realm.subscriptions().update([](realm::MutableSyncSubscriptionSet& subs) {
+    update_success = synced_realm.subscriptions().update([](realm::MutableSyncSubscriptionSet& subs) {
         subs.add<FooObject>("foo-strings", [](auto& obj) {
             return obj.str_col != "alex"; // sync all objects where name does not equal 'alex'
         });
-    });
+    }).get_future().get();
 
     auto person = FooObject();
     person._id = 1;
@@ -77,44 +72,25 @@ realm::task<realm::db_config> run_realm() {
         std::cout << "property changed" << std::endl;
     });
 
-    co_await wait_for_sync_uploads(user);
-    co_await wait_for_sync_downloads(user);
+    wait_for_sync_uploads(user).get_future().get();
+    wait_for_sync_downloads(user).get_future().get();
 
     synced_realm.write([&synced_realm, &person]() {
         person.str_col = "sarah";
     });
 
-    co_await wait_for_sync_uploads(user);
-    co_await wait_for_sync_downloads(user);
+    wait_for_sync_uploads(user).get_future().get();
+    wait_for_sync_downloads(user).get_future().get();
 
     synced_realm.write([&synced_realm, &person]() {
         person.str_col = "bob";
     });
 
-    co_await wait_for_sync_uploads(user);
-    co_await wait_for_sync_downloads(user);
-
-    co_return flx_sync_config;
+    wait_for_sync_uploads(user).get_future().get();
+    wait_for_sync_downloads(user).get_future().get();
 }
 
 int main() {
-    task = run_realm();
-    while (task.handle.done() == false) { }
-
-    auto config = task.await_resume();
-    realm::notification_token token;
-    auto db = realm::open<FooObject>(config);
-    auto obj = *db.objects<FooObject>()[0];
-
-    realm::util::EventLoop::main().perform([&token, &obj]() {
-        token = obj.observe<FooObject>([](auto&& change) {
-            std::cout << "property changed async" << std::endl;
-        });
-    });
-
-    realm::util::EventLoop::main().run_until([]() {
-        return false;
-    });
-
+    run_realm();
     return 0;
 }
