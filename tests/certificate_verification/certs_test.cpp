8fe6298b17a77b9017b3d2a1ccbd9759e9691354
Lee Maguire <lee.maguire@mongodb.com>
2023-10-03 16:37:37 +0100

    Test if certs are included in soong build

---
+++ tests/certificate_verification/certs_test.cpp
@@ -0,0 +1,25 @@
+#include "certs_test.hpp"
+
+#include <iostream>
+#include <cpprealm/sdk.hpp>
+#include <cpprealm/internal/generic_network_transport.hpp>
+
+int main(int argc, char *argv[]) {
+
+    static realm::internal::DefaultTransport transport;
+
+    realm::app::Request r;
+    r.url = "https://www.mongodb.com";
+
+    std::promise<realm::app::Response> p;
+    transport.send_request_to_server(std::move(r),
+                                     [&p](auto&& response){
+                                         p.set_value(std::move(response));
+                                     });
+    auto res = p.get_future().get();
+
+    std::cout << res.http_status_code << "\n";
+    std::cout << res.body << std::endl;
+
+    return 0;
+}
---
+++ tests/certificate_verification/certs_test.hpp
@@ -0,0 +1,7 @@
+#ifndef REALM_CERTS_TEST_HPP
+#define REALM_CERTS_TEST_HPP
+
+
+int main(int argc, char *argv[]);
+
+#endif//REALM_CERTS_TEST_HPP
