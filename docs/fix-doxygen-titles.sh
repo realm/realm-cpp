1100e1f705f85a1838641684ba5cb535ba8a91d6
Dachary Carey <dachary.carey@mongodb.com>
2023-11-02 10:36:37 -0400

    Remove the root dir stuff

--- docs/fix-doxygen-titles.sh
+++ docs/fix-doxygen-titles.sh
@@ -1,18 +1,12 @@
 #!/bin/bash -ex
 
 # Output this to a temp directory because we can't write to the html directory
-root_directory=$(dirname "$0")
-mkdir "$root_directory"/modified-doxygen-output
-
 # Assume doxygen was run first
-pushd "`dirname "$0"`"/html
 cp -R html modified-doxygen-output
 
-popd
-
-find ."$root_directory"/modified-doxygen-output -name "*.html" | while read ln
+find ./modified-doxygen-output -name "*.html" | while read ln
 do
   # Make the output SEO friendly by converting the "div" title to the proper "h1"
   sed -i -e 's|<div class="title">\([^<]*\)</div>|<h1>\1</h1>|' "$ln"
 done
-find ."$root_directory"/modified-doxygen-output -iname "*.html-e" | xargs rm
+find ./modified-doxygen-output -iname "*.html-e" | xargs rm
