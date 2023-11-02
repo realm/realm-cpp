b6937cb33a4f2ea099842dbde3b4f15b9a594720
Dachary <dachary.carey@mongodb.com>
2023-11-02 12:01:49 -0400

    Fix Doxygen publishing pipeline (#108)

--- docs/fix-doxygen-titles.sh
+++ docs/fix-doxygen-titles.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -ex
 
+# Output this to a temp directory because we can't write to the html directory
 # Assume doxygen was run first
-pushd "`dirname "$0"`"/html
+pushd "`dirname "$0"`"
+cp -R html modified-doxygen-output
 
-find . -name "*.html" | while read ln
+find ./modified-doxygen-output -name "*.html" | while read ln
 do
   # Make the output SEO friendly by converting the "div" title to the proper "h1"
   sed -i -e 's|<div class="title">\([^<]*\)</div>|<h1>\1</h1>|' "$ln"
 done
-find . -iname "*.html-e" | xargs rm
+find ./modified-doxygen-output -iname "*.html-e" -exec rm '{}' \;
 
 popd
