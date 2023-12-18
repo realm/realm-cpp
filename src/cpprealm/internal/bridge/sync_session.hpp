c90f1cfc4bf6c9926269c18986b49ad3b1bfe2fa
Lee Maguire <lee.maguire@mongodb.com>
2023-12-18 19:37:11 +0000

    Add pause resume

--- src/cpprealm/internal/bridge/sync_session.hpp
+++ src/cpprealm/internal/bridge/sync_session.hpp
@@ -10,17 +10,30 @@ namespace realm {
     class SyncSession;
     namespace internal::bridge {
         struct status;
+        struct realm;
 
         struct sync_session {
+
             enum class state {
                 active,
                 dying,
                 inactive,
                 waiting_for_access_token,
+                paused
             };
 
-            sync_session(const std::shared_ptr<SyncSession> &);
+            enum class connection_state {
+                disconnected,
+                connecting,
+                connected
+            };
+
+            // The session's current state.
             enum state state() const;
+
+            // The session's current connection state.
+            enum connection_state connection_state() const;
+
             // Register a callback that will be called when all pending uploads have completed.
             // The callback is run asynchronously, and upon whatever thread the underlying sync client
             // chooses to run it on.
@@ -29,14 +42,63 @@ namespace realm {
             // Register a callback that will be called when all pending downloads have been completed.
             // Works the same way as `wait_for_upload_completion()`.
             void wait_for_download_completion(std::function<void(status)>&& callback);
+
             // Register a callback that will be called when all pending uploads have completed.
             // The callback is run asynchronously, and upon whatever thread the underlying sync client
             // chooses to run it on.
             std::future<void> wait_for_upload_completion();
+
             // Register a callback that will be called when all pending downloads have been completed.
             std::future<void> wait_for_download_completion();
+
+            /**
+            Temporarily suspend syncronization and disconnect from the server.
+
+            The session will not attempt to connect to Atlas App Services until `resume`
+            is called or the Realm file is closed and re-opened.
+             */
+            void pause();
+
+            /**
+            Resume syncronization and reconnect to Atlas App Services after suspending.
+
+            This is a no-op if the session was already active or if the session is invalid.
+            Newly created sessions begin in the Active state and do not need to be resumed.
+             */
+            void resume();
+
+            /**
+            Request an immediate reconnection to the server if the session is disconnected.
+
+            Realm automatically reconnects after disconnects with an exponential backoff,
+            which is reset when the reachability handler reports a network status change.
+            In some scenarios an application may wish to skip the reconnect delay, such as
+            when an application receives a wake from background notification, which can be
+            done by calling this method. Calling this method is never required.
+
+            This method is asynchronous and merely skips the current reconnect delay, so
+            the connection state will still normally be disconnected immediately after
+            calling it.
+
+            Has no effect if the session is currently connected.
+             */
+            void reconnect();
+
+            /**
+             Registers a callback that is invoked when the underlying sync session changes
+             its connection state
+             */
+            uint64_t observe_connection_change(std::function<void(enum connection_state old_state, enum connection_state new_state)>&& callback);
+
+            /**
+             Unregister a previously registered notifier. If the token is invalid,
+             this method does nothing.
+             */
+            void unregister_connection_change_observer(uint64_t token);
         private:
             std::weak_ptr<SyncSession> m_session;
+            sync_session(const std::shared_ptr<SyncSession> &);
+            friend struct internal::bridge::realm;
         };
     }
 }
