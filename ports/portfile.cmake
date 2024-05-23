9d80e5cc888f0bf20f0508cb85c26b0c01af0f73
Lee Maguire <lee.maguire@mongodb.com>
2024-05-23 21:07:40 +0100

    revert vcpkg changes

--- ports/portfile.cmake
+++ ports/portfile.cmake
@@ -1,6 +1,14 @@
 cmake_path(GET CMAKE_CURRENT_LIST_DIR PARENT_PATH SOURCE_PATH)
 
-set(CPPREALM_CMAKE_OPTIONS -DREALM_CPP_NO_TESTS=ON)
+find_program(GIT git)
+
+vcpkg_execute_required_process(
+  COMMAND ${GIT} submodule update --init --recursive
+  WORKING_DIRECTORY ${SOURCE_PATH}
+  LOGNAME submodules
+)
+
+set(CPPREALM_CMAKE_OPTIONS -DREALM_CPP_NO_TESTS=ON -DREALM_CORE_SUBMODULE_BUILD=OFF)
 
 if (ANDROID OR WIN32 OR CMAKE_SYSTEM_NAME STREQUAL "Linux")
     list(APPEND CPPREALM_CMAKE_OPTIONS -DREALM_USE_SYSTEM_OPENSSL=ON)
@@ -14,7 +22,9 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+
 vcpkg_cmake_config_fixup(PACKAGE_NAME "cpprealm" CONFIG_PATH "cmake")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "Realm" CONFIG_PATH "share/cmake/Realm")
 
 file(READ ${CURRENT_PACKAGES_DIR}/debug/include/cpprealm/internal/bridge/bridge_types.hpp DEBUG_TYPE_HEADER_CONTENTS)
 set(REGEX_PATTERN "\\{([^()]*)\\}")
@@ -38,7 +48,10 @@ string(REGEX REPLACE "\\{([^()]*)\\}" "
 
 file(WRITE ${CURRENT_PACKAGES_DIR}/include/cpprealm/internal/bridge/bridge_types.hpp "${MODIFIED_HEADER}")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
 
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
